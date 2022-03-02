2200d81c47edbddef7e86b5222480f9cf5c0c7e3
Gaurika Gupta <84630728+Gaurika-Gupta@users.noreply.github.com>
2022-03-02 17:12:18 +0530

    Create main.js

---
+++ Calculators/Scientific Calculator with UI/main.js
@@ -0,0 +1,79 @@
+var screen=document.querySelector('#screen');
+var btn=document.querySelectorAll('.btn');
+
+    /*============ For getting the value of btn, Here we use for loop ============*/
+    for(item of btn) {
+        item.addEventListener('click',(e)=>{
+            btntext=e.target.innerText;
+
+            if(btntext =='×')
+            {
+                btntext= '*';
+            }
+
+            if(btntext=='÷')
+            {
+                btntext='/';
+            }
+            screen.value+=btntext;
+        });
+    }
+
+    function sin() {
+        screen.value=Math.sin(screen.value);
+    }
+
+    function cos()
+    {
+        screen.value=Math.cos(screen.value);
+    }
+
+    function tan()
+    {
+        screen.value=Math.tan(screen.value);
+    }
+
+    function pow()
+    {
+        screen.value=Math.pow(screen.value,2);
+    }
+
+    function sqrt()
+    {
+        screen.value=Math.sqrt(screen.value,2);
+    }
+
+    function log()
+    {
+        screen.value=Math.log(screen.value);
+    }
+
+    function pi()
+    {
+        screen.value= 3.14159265359;
+    }
+
+    function e()
+    {
+        screen.value=2.71828182846;
+    }
+
+    function fact()
+    {
+        var i, num, f;
+        f=1
+        num=screen.value;
+        for(i=1; i<=num; i++)
+        {
+            f=f*i;
+        }
+
+        i=i-1;
+
+        screen.value=f;
+    }
+
+    function backspc()
+    {
+        screen.value=screen.value.substr(0,screen.value.length-1);
+    }
