d96df4bca0c6a704a3d7693399aee1927e0467a5
riyasai22 <80235375+riyasai22@users.noreply.github.com>
2022-03-07 23:54:13 +0530

    Update app.js

--- Calculators/Euclidean_Algorithm_Calculator/app.js
+++ Calculators/Euclidean_Algorithm_Calculator/app.js
@@ -6,15 +6,30 @@ function calculate(e){
     e.preventDefault();
     let a=document.querySelector(".num1").value;
     let b=document.querySelector(".num2").value;
+
     let res=gcd(a,b);
-    const results=document.querySelector(".results");
+    let res2=lcm(a,b);
+
+    const gcdFinal=document.querySelector(".gcd-final");
+    const lcmFinal=document.querySelector(".lcm-final");
+
     const gcdVal=document.querySelector(".gcd");
+    const lcmVal=document.querySelector(".lcm");
     const num1=document.querySelector(".n1");
     const num2=document.querySelector(".n2");
+    const num3=document.querySelector(".n3");
+    const num4=document.querySelector(".n4");
+
     gcdVal.innerText=res;
+    lcmVal.innerText=res2;
+
     num1.innerText=a;
     num2.innerText=b;
-    results.classList.add("show");
+    num3.innerText=a;
+    num4.innerText=b;
+
+    gcdFinal.classList.add("show");
+    lcmFinal.classList.add("show");
 }
 function gcd(a,b){
     var r;
@@ -24,4 +39,7 @@ function gcd(a,b){
         b=r
     }
     return b
+}
+function lcm(a,b){
+    return a*b/gcd(a,b);
 }
