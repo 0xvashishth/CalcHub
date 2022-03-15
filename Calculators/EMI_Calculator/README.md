1270c491887a0e31a9bbd374c1e89fbacb4b5951
vaslal-324 <radhabaim14@gmail.com>
2022-03-15 20:34:04 +0530

    emi-calc

--- Calculators/EMI_Calculator/README.md
+++ Calculators/EMI_Calculator/README.md
@@ -1,5 +1,34 @@
-# EMI CALCULATOR
-The calculator is built using html, css and javascript.<br/>
-This is used to calculate EMI with the help of the formula :<br/><br/>
-### EMI = (P * r * (1 + r)^n)/ ((1 + r)^n - 1) <br/>
+# EMI CALCULATOR (HOME LOAN)
+<h4>Built using : </h4>
+<ul>
+<li>Html</li> 
+<li>Css</li>
+<li>Javascript</li>
+</ul>
+<h4>Formula used :</h4><br/>
+
+<h3>
+<blockquote cite="https://www.bajajhousingfinance.in/how-to-calculate-home-loan-emi">
+EMI = (P * r * (1 + r)^n)/ (((1 + r)^n) - 1) <br/>
 where r is rate/(12*100)
+</blockquote>
+</h3>
+<br/><br/>
+
+<h4>Before Computation: </h4>  
+<img src="C:\Users\VASUDHA\Desktop\VASUDHA\Calculators\CalcHub\Calculators\EMI_Calculator\SS_1.png">
+<br/>
+<br/>
+<h4>After Computation: </h4>
+<img src="C:\Users\VASUDHA\Desktop\VASUDHA\Calculators\CalcHub\Calculators\EMI_Calculator\SS_2.png">
+
+<br/><br/>
+<h4>Functionalities : </h4>
+<ul>
+    <li>Calculates EMI for home loan for values entered by the user.</li>
+    <li>Can be used as a browser extension after minor modifications.</li>
+</ul>
+
+
+
+
--- Calculators/EMI_Calculator/index.js
+++ Calculators/EMI_Calculator/index.js
@@ -4,19 +4,25 @@ function calculate(){
     const rate = formid['rate'].value;
     const tenure = formid['tenure'].value;
 
-    principal_float = parseFloat(principal);
-    rate_float = parseFloat(rate);
-    tenure_float = parseFloat(tenure);
+    const principal_float = parseFloat(principal);
+    const rate_float = parseFloat(rate); //rate in %
+    const tenure_int = parseInt(tenure); // tenure in months , eg: 2 yrs=> 240 months 
 
-    rate_month = rate_float/12*100;
-    nr = 1;
-    for(let i=1; i<=tenure_float; i++)
+    const rate_month = rate_float/ (12 * 100); //rate per moth => if rate is 10%, rate per month = 10/12 = 0.833%
+    var nr = 1;
+    for(var i=1; i<=tenure_int; i++)
     nr*=(1+rate_month);
+    //alert(nr);
 
-    res = (principal * rate_month * nr)/(nr - 1);
+    var res = (principal * rate_month * nr)/(nr - 1);
+    res_final = res.toFixed(2);
     //alert(res);
+    
+   /*const interest = principal_float * rate_float * tenure_float / 100 ;
+   const amt = interest +  principal_float;
+   const res = amt / tenure_float;*/
 
-    document.getElementById('res').innerHTML = res;
+    document.getElementById('res').innerHTML = res_final;
 
 }
 
