80b55b4b7b7f62c4380d93a9e07c895b99b39eaf
Kushal-2002 <mukherjeekushal123@gmail.com>
2022-03-28 10:19:41 +0530

    code refactored

--- Calculators/PasswordStrength_Calculator/script.js
+++ Calculators/PasswordStrength_Calculator/script.js
@@ -1,38 +1,3 @@
-// function time() {
-//     var wakeHours = Number(document.getElementById("hour").value);
-//     var wakeMinutes = Number(document.getElementById("minute").value);
-//     var wakeMeridian = document.getElementById("meridian").value;
-//     var totalWakeMinutes = wakeHours * 60 + wakeMinutes;
-
-//     const sleep = [
-//       (totalWakeMinutes - 90 * 6-15) / 60,
-//       (totalWakeMinutes - 90 * 5-15) / 60,
-//       (totalWakeMinutes - 90 * 4-15) / 60,
-
-//       (totalWakeMinutes - 90 * 3-15) / 60,
-//       (totalWakeMinutes - 90 * 2-15) / 60,
-//       (totalWakeMinutes - 90 * 1-15) / 60,
-//     ];
-//     const newSleep = sleep.map((hour) => {
-//       if (hour < 1) {
-//         if (wakeMeridian === "am") {
-//           return Math.floor(hour+12)+":"+ (((hour + 12)-Math.floor(hour + 12))*60 === 0 ? '00' : ((hour + 12)-Math.floor(hour + 12))*60) + "pm";
-//         } else if (wakeMeridian === "pm") {
-//           return Math.floor(hour+12)+":"+ (((hour + 12)-Math.floor(hour + 12))*60 === 0 ? '00' : ((hour + 12)-Math.floor(hour + 12))*60) + "am";
-//         }
-//       } else {
-//         if (wakeMeridian === "am") {
-//           return Math.floor(hour)+":"+ (((hour)-Math.floor(hour))*60 === 0 ? '00' : ((hour)-Math.floor(hour))*60) + "am";
-//         } else if (wakeMeridian === "pm") {
-//           return Math.floor(hour)+":"+ (((hour)-Math.floor(hour))*60 === 0 ? '00' : ((hour)-Math.floor(hour))*60) + "pm";
-//         }
-//       }
-//     });
-
-//     document.getElementById('sleepTime').innerHTML="Suggested Bed Time --> "+newSleep[0]+" or "+newSleep[1];
-//     document.getElementById('otherSleepTime').innerHTML=newSleep[2]+","+newSleep[3]+","+newSleep[4]+","+  newSleep[5] + " are the timings in which you can also sleep.";
-//     document.getElementById('sleep').innerHTML="If you wake up at one of these times, you’ll rise in between 90-minute sleep cycles. A good night’s sleep consists of 5-6 complete sleep cycles.";
-//   }
 function pass() {
   var p = document.getElementById("password").value;
   var upperCase = 0;
@@ -58,30 +23,23 @@ function pass() {
       special = 1;
     }
   }
-  // console.log((upperCase+lowerCase+number+special+len)*20);
+  var arr = [upperCase, lowerCase, special, number];
+  var arr1 = ["uppercase", "lowercase", "special", "numeric"];
   var strengthPercent = (upperCase + lowerCase + number + special + len) * 20;
   document.getElementById(
     "strength"
   ).innerHTML = `Strength of your password is ${strengthPercent}%`;
   document.getElementById("strength").style.color =
     strengthPercent === 100 ? "green" : "red";
-  if (upperCase === 0) {
-    document.getElementById("uppercase").innerHTML =
-      "Your password must include an uppercase character";
+
+  for (var i = 0; i < 4; i++) {
+    if (arr[i] == 0) {
+      document.getElementById(
+        arr1[i]
+      ).innerHTML = `Your password must include an ${arr1[i]} character`;
+    }
   }
- 
-  if (lowerCase === 0) {
-    document.getElementById("lowercase").innerHTML =
-      "Your password must include a lowercase character";
-  }
-  if (special === 0) {
-    document.getElementById("special").innerHTML =
-      "Your password must include a special character";
-  }
-  if (number === 0) {
-    document.getElementById("number").innerHTML =
-      "Your password must include a number ";
-  }
+
   if (len === 0) {
     document.getElementById("length").innerHTML =
       "Your password must be atleast 12 characters long ";
