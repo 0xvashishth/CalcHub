77559c552c75ed0293a73eb2e1e05b09a19564bf
Ashish4321-hub <anmol2001anu@gmail.com>
2022-05-10 01:04:40 +0530

    changes in html files

--- Calculators/Area_Calculator/README.md
+++ Calculators/Area_Calculator/README.md
@@ -29,4 +29,4 @@ area of square = side^2
 
 ![area](https://user-images.githubusercontent.com/88235823/167244213-ea9fe6a2-c417-4723-84c9-d7ff10c3b385.PNG)
 
-![araea](https://user-images.githubusercontent.com/88235823/167244217-88d5bda9-1d1b-4dd9-9905-f2777b24862b.PNG)
+![image](https://user-images.githubusercontent.com/88235823/167483881-bc2d9094-a8bc-4f56-b26b-9e4be040b31c.png)
--- Calculators/Area_Calculator/index.js
+++ Calculators/Area_Calculator/index.js
@@ -1,14 +1,14 @@
 
 
 function rec(){
-  var d=document.querySelector(".len").value;
-var e=document.querySelector(".brea").value;
-  document.querySelector(".recans").innerHTML="Area of rectangle is " + d*e +"m^2";
+  var d1=document.querySelector(".len").value;
+var e1=document.querySelector(".brea").value;
+  document.querySelector(".recans").innerHTML="Area of rectangle is " + d1*e1 +"m^2";
 }
 function cir(){
   
-var f=document.querySelector(".rad").value;
-  document.querySelector(".cirans").innerHTML="Area of circle is " + 3.14*f*f+"m^2";
+var fg=document.querySelector(".radc").value;
+  document.querySelector(".cirans").innerHTML="Area of circle is " + 3.14*fg*fg+"m^2";
 }
 function sq(){
   
@@ -16,46 +16,46 @@ function sq(){
     document.querySelector(".sqans").innerHTML="Area of square is " + o*o+"m^2";
   }
   function cylinder(){
-    var d=document.querySelector(".height").value;
-  var e=document.querySelector(".rad").value;
-    document.querySelector(".cylans").innerHTML="Area of Cylinder is " + 2*3.14*e*(e+ d) +"m^2";
+    var dy=document.querySelector(".heightcy").value;
+  var ey=document.querySelector(".radcy").value;
+    document.querySelector(".cylans").innerHTML="Area of Cylinder is " + 2*3.14*ey*(ey+ dy) +"m^2";
   }
   function cone(){
-    var d=document.querySelector(".rad").value;
-  var e=document.querySelector(".height").value;
-    document.querySelector(".coneans").innerHTML="Area of cone is " + 3.14*d*(d + e) +"m^2";
+    var d2=document.querySelector(".radon").value;
+  var e2=document.querySelector(".heighton").value;
+    document.querySelector(".coneans").innerHTML="Area of cone is " + 3.14*d2*(d2 + e2) +"m^2";
   }
   function cube(){
-    var d=document.querySelector(".len").value;
+    var dx=document.querySelector(".lencu").value;
   
-    document.querySelector(".cubeans").innerHTML="Area of cube is " + 6*d*d+"m^2";
+    document.querySelector(".cubeans").innerHTML="Area of cube is " + 6*dx*dx+"m^2";
   }
   function ellipse(){
-    var d=document.querySelector(".len").value;
-  var e=document.querySelector(".brea").value;
-    document.querySelector(".ellipseans").innerHTML="Area of ellipse is " + 3.14*d*e +"m^2";
+    var d4=document.querySelector(".lenx").value;
+  var e4=document.querySelector(".breax").value;
+    document.querySelector(".ellipseans").innerHTML="Area of ellipse is " + 3.14*d4*e4 +"m^2";
   }
   function hemisphere(){
-var f=document.querySelector(".rad").value;
+var fo=document.querySelector(".radmi").value;
     
-    document.querySelector(".hemians").innerHTML="Area of Hemisphere is " + 3*3.14*f*f +"m^2";
+    document.querySelector(".hemians").innerHTML="Area of Hemisphere is " + 3*3.14*fo*fo +"m^2";
   }
   function parallelogram(){
-    var d=document.querySelector(".height").value;
-  var e=document.querySelector(".base").value;
-    document.querySelector(".paraans").innerHTML="Area of parallelogram is " + d*e +"m^2";
+    var d5=document.querySelector(".heightp").value;
+  var e5=document.querySelector(".basep").value;
+    document.querySelector(".paraans").innerHTML="Area of parallelogram is " + d5*e5 +"m^2";
   }
   function rectangularprism(){
-    var d=document.querySelector(".len").value;
-  var e=document.querySelector(".brea").value;
+    var d6=document.querySelector(".lenpri").value;
+  var e6=document.querySelector(".breapri").value;
 
-  var f=document.querySelector(".height").value;
-    document.querySelector(".recprians").innerHTML="Area of rectangularprism is " +2*( d*e +e*f+ f*d) +"m^2";
+  var f6=document.querySelector(".heightpri").value;
+    document.querySelector(".recprians").innerHTML="Area of rectangularprism is " +2*( d6*e6 +e6*f6+ f6*d6) +"m^2";
   }
   function sphere(){
-    var f=document.querySelector(".rad").value;
+    var fi=document.querySelector(".radsp").value;
     
-    document.querySelector(".sphans").innerHTML="Area of sphere is " + 4*3.14*f*f +"m^2";
+    document.querySelector(".sphans").innerHTML="Area of sphere is " + 4*3.14*fi*fi +"m^2";
   }
   function trapezium(){
     var dt=document.querySelector(".lena").value;
@@ -65,7 +65,7 @@ var f=document.querySelector(".rad").value;
       document.querySelector(".trapans").innerHTML="Area of trapezium is " + (0.5*iy*ft) +"m^2";
   }
   function triangle(){
-    var d=document.querySelector(".height").value;
-  var e=document.querySelector(".base").value;
-    document.querySelector(".trians").innerHTML="Area of trianglris " + 0.5*d*e +"m^2";
+    var dl=document.querySelector(".heighttri").value;
+  var el=document.querySelector(".basetri").value;
+    document.querySelector(".trians").innerHTML="Area of trianglris " + 0.5*dl*el +"m^2";
   }
