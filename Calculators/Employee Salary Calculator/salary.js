9e2d0bb484d69b8bc151b94499f26d7d1d3c69b4
PothugantiChamili <93437720+PothugantiChamili@users.noreply.github.com>
2022-05-12 22:11:14 +0530

    javascript file

--- Calculators/Employee Salary Calculator/salary.js
+++ Calculators/Employee Salary Calculator/salary.js
@@ -1,10 +1,10 @@
- function sal(){
+  function sal(){
        var num1 = document.getElementById("a").value;
        var num2 = document.getElementById("b").value;
        
        var check = (num2 / 100) * num1;
        var result = num1 - check;
-       document.getElementById("res").innerHTML = "Net Salary: "+result.toFixed();
+       document.getElementById("res").innerHTML = "Money paid For Tax : "+check.toFixed()+"<br> Net Salary: "+result.toFixed();
 
 }
-                
+                      
