799f98d20f83dcbc07c0f2dd8c0722980878ac7a
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2022-05-15 19:45:17 +0530

    Update script.js

--- Calculators/Temperature_Calculator/script.js
+++ Calculators/Temperature_Calculator/script.js
@@ -1,6 +1,7 @@
 const valueCelsius = document.getElementById("celsius");
 const valueKelvin = document.getElementById("kelvin");
 const valueFahrenheit = document.getElementById("fahrenheit");
+const valueRankine = document.getElementById("rankine");
 
 const temp = document.getElementsByClassName("value");
 
@@ -13,12 +14,19 @@ for (let i = 0; i < temp.length; i++) {
     if (e.target.name == "celsius") {
       valueFahrenheit.value = value * 1.8 + 32;
       valueKelvin.value = value + 273.15;
+      valueRankine.value = (value * (9/5)) + 491.67;
     } else if (e.target.name == "fahrenheit") {
       valueCelsius.value = (value - 32) / 1.8;
       valueKelvin.value = (value - 32) / 1.8 + 273.15;
-    } else {
+      valueRankine.value = value - 459.67;
+    } else if (e.target.name == "kelvin"){
       valueCelsius.value = value - 273.15;
       valueFahrenheit.value = (value - 273.15) * 1.8 + 32;
+      valueRankine.value = value * (9/5);
+    } else if (e.target.name == "rankine"){
+      valueFahrenheit.value = value + 459.67;
+      valueCelsius.value = (value - 491.67) * (5/9);
+      valueKelvin.value = value * (5/9);
     }
   });
 }
