1e31b7570f20dd3e46f7449fbe7cb9122ee7fa94
D.Poriya <dharmeshkporiya@gmail.com>
2022-10-30 14:34:43 +0530

    validation added in simple interest calculator

--- Calculators/Simple_Interest/script.js
+++ Calculators/Simple_Interest/script.js
@@ -12,6 +12,41 @@ let t = 0;
 let interest = 0;
 let amount = 0;
 
+function resetInputs() {
+    principal.value = '';
+    rate.value = '';
+    time.value = '';
+    resultInterest.innerText = '';
+    resultAmount.innerText = '';
+    resultElement.classList.add('hide');
+    document.getElementById('errorMsgP').innerText = document.getElementById('errorMsgR').innerText = document.getElementById('errorMsgN').innerText = '';
+    document.getElementById('errorMsgP').removeAttribute("class", "alert alert-warning");
+    document.getElementById('errorMsgR').removeAttribute("class", "alert alert-warning");
+    document.getElementById('errorMsgN').removeAttribute("class", "alert alert-warning");
+}
+
+function checkNumber(id, eid) {
+    let p = document.getElementById(id).value;
+    if (p < 0) {
+        document.getElementById(eid).innerText = "Please enter a positive number";
+        document.getElementById(eid).setAttribute("class", "alert alert-warning");
+        return;
+    } else {
+        document.getElementById(eid).innerText = "";
+        document.getElementById(eid).removeAttribute("class", "alert alert-warning");
+    }
+}
+
+function checkNumberP() {
+    checkNumber('principal', 'errorMsgP');
+}
+function checkNumberR() {
+    checkNumber('rate', 'errorMsgR');
+}
+function checkNumberN() {
+    checkNumber('time', 'errorMsgN');
+}
+
 principal.addEventListener('input', (e) => {
     resultElement.classList.add('hide');
 })
@@ -22,7 +57,7 @@ const calculateInterest = () => {
     r = parseFloat(rate.value);
     t = parseFloat(time.value);
 
-    interest = ((p*r*t)/100);
+    interest = ((p * r * t) / 100);
     resultInterest.innerText = `₹ ${interest.toFixed(2)}`;
 }
 
@@ -35,7 +70,10 @@ const calculateAmount = () => {
 console.log("form")
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
+    if (0 > parseInt(principal.value) || 0 > rate.value || 0 > time.value) {
+        return;
+    }
     resultElement.classList.remove('hide');
     calculateInterest();
     calculateAmount();
